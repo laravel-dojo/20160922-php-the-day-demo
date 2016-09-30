4927e6f29987c984efb9ebb17cc3db7d2b275553
Shengyou Fan <shengyoufan@gmail.com>
2016-09-30 23:32:44 +0800

    啟動 Laravel Tracy Debugger

--- bootstrap.php
+++ bootstrap.php
@@ -6,3 +6,6 @@ include __DIR__.'/inc/database.php';
 include __DIR__.'/libs/my_string_mutator.php';
 include __DIR__.'/libs/date_mutator.php';
 include __DIR__.'/helpers/my_functions.php';
+
+use Recca0120\LaravelTracy\Tracy;
+Tracy::instance();
