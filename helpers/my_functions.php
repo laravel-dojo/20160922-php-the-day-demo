14f4e3499eab3836620a56ca6a9d8492ea1937ab
Shengyou Fan <shengyoufan@gmail.com>
2016-10-01 01:02:38 +0800

    優化一下 helper 的寫法

--- helpers/my_functions.php
+++ helpers/my_functions.php
@@ -2,14 +2,18 @@
 
 use Recca0120\LaravelTracy\Tracy;
 
-function copyright()
-{
-    return 'Copyright &copy; Laravel 道場示範教材 '.date('Y');
+if (! function_exists('copyright')) {
+    function copyright()
+    {
+        return 'Copyright &copy; Laravel 道場示範教材 '.date('Y');
+    }
 }
 
-function sql($sql)
-{
-    $tracy = Tracy::instance();
-    $databasePanel = $tracy->getPanel('database');
-    $databasePanel->logQuery($sql);
+if (! function_exists('sql')) {
+    function sql($sql)
+    {
+        $tracy = Tracy::instance();
+        $databasePanel = $tracy->getPanel('database');
+        $databasePanel->logQuery($sql);
+    }
 }
