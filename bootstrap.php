a18ae4078e8c030d57c3f604ac111d4398a8a618
Shengyou Fan <shengyoufan@gmail.com>
2016-10-01 00:13:37 +0800

    透過 Dotenv 載入 .env 檔

--- bootstrap.php
+++ bootstrap.php
@@ -2,10 +2,16 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-include __DIR__.'/inc/database.php';
 include __DIR__.'/libs/my_string_mutator.php';
 include __DIR__.'/libs/date_mutator.php';
 include __DIR__.'/helpers/my_functions.php';
 
+use Dotenv\Dotenv;
 use Recca0120\LaravelTracy\Tracy;
+
+$dotenv = new Dotenv(__DIR__);
+$dotenv->load();
+
+include __DIR__.'/inc/database.php';
+
 Tracy::instance();
