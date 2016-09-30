59995a58e2124cc43f0fe11b06ff2d6c70596596
Shengyou Fan <shengyoufan@gmail.com>
2016-10-01 00:58:33 +0800

    將 helpers 資料夾內的檔案以 files autoload

--- bootstrap.php
+++ bootstrap.php
@@ -4,8 +4,6 @@ date_default_timezone_set('Asia/Taipei');
 
 require __DIR__.'/vendor/autoload.php';
 
-include __DIR__.'/helpers/my_functions.php';
-
 use Carbon\Carbon;
 use Dotenv\Dotenv;
 use Recca0120\LaravelTracy\Tracy;
