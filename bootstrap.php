f8e0510cf86ec116c533d9dcacb1890ba85d3383
Shengyou Fan <shengyoufan@gmail.com>
2016-10-01 01:01:38 +0800

    把 inc/database.php 移除

--- bootstrap.php
+++ bootstrap.php
@@ -11,8 +11,13 @@ use Recca0120\LaravelTracy\Tracy;
 $dotenv = new Dotenv(__DIR__);
 $dotenv->load();
 
-include __DIR__.'/inc/database.php';
-
 Tracy::instance();
 
 Carbon::setLocale('zh-TW');
+
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_PORT', getenv('DB_PORT'));
+define('DB_DATABASE', getenv('DB_DATABASE'));
+define('DB_CHARSET', getenv('DB_CHARSET'));
+define('DB_USERNAME', getenv('DB_USERNAME'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
--- inc/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_HOST', getenv('DB_HOST'));
-define('DB_PORT', getenv('DB_PORT'));
-define('DB_DATABASE', getenv('DB_DATABASE'));
-define('DB_CHARSET', getenv('DB_CHARSET'));
-define('DB_USERNAME', getenv('DB_USERNAME'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
